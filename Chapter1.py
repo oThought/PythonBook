1bb255b871a9a51de920e77a921aef5d28514ab7
Fraser <YangTurnerF@wilsonsschool.sutton.sch.uk>
2023-01-22 17:04:45 +0000

    Chapter 1

---
+++ Chapter1.py
@@ -0,0 +1,48 @@
+# Q1
+
+a = 10 * 6 + 5 * 3
+b = 10 * (6 + 5) * 3
+c = 33 % 7
+
+print(a, b, c)
+
+# Q2
+
+a = round(6.7 / 3, 3)
+
+print(a)
+
+# Q3
+
+highestScore = 25
+playerName = "Davina"
+height, base = 4.5, 6
+areaTriangle = height * base / 2
+
+print(highestScore, playerName, height, base, areaTriangle)
+
+# Q4
+
+counter = 0
+counter += 1
+housePrice = 1
+housePrice *= 2
+hits = 2
+penalty = 1
+hits -= penalty
+totalCostMeal = 3
+totalCostMeal /= 3
+totalCostMeal = round(totalCostMeal)
+
+# Expected: 1, 2, 1, 1
+print(counter, housePrice, hits, totalCostMeal)
+
+# Q5
+
+print("More pain, more gain")
+print("That's True")
+print("".join(["Pelham", "123"]))
+print("How will this", 
+      "statement", 
+      "be printed?"
+)
